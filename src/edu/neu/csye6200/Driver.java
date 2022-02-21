cd1097a72ce20a543b6bcf764ef583069c032af5
Jayanth Vakkalagadda <vakkalagadda.j@northeastern.edu>
2022-02-21 00:15:50 -0500

    output

--- src/edu/neu/csye6200/Driver.java
+++ src/edu/neu/csye6200/Driver.java
@@ -7,3 +7,82 @@ public class Driver {
 	}
 	
 }
+
+
+/*
+
+ CONSOLE OUTPUT
+
+*
+*Sorting StudentRoster based on ID*
+
+id: 10, age: 28, firstName: rama, lastName: tej, gpa: 3.1
+id: 20, age: 27, firstName: patel, lastName: Munaf, gpa: 3.9
+id: 30, age: 30, firstName: sehwag, lastName: virender, gpa: 3.2
+id: 40, age: 29, firstName: peters, lastName: daniel, gpa: 3.8
+id: 50, age: 26, firstName: sharma, lastName: rohit, gpa: 2.4
+
+
+*Sorting StudentRoster based on Age*
+
+id: 50, age: 26, firstName: sharma, lastName: rohit, gpa: 2.4
+id: 20, age: 27, firstName: patel, lastName: Munaf, gpa: 3.9
+id: 10, age: 28, firstName: rama, lastName: tej, gpa: 3.1
+id: 40, age: 29, firstName: peters, lastName: daniel, gpa: 3.8
+id: 30, age: 30, firstName: sehwag, lastName: virender, gpa: 3.2
+
+
+*Sorting StudentRoster based on LastName*
+
+id: 40, age: 29, firstName: peters, lastName: daniel, gpa: 3.8
+id: 20, age: 27, firstName: patel, lastName: Munaf, gpa: 3.9
+id: 50, age: 26, firstName: sharma, lastName: rohit, gpa: 2.4
+id: 10, age: 28, firstName: rama, lastName: tej, gpa: 3.1
+id: 30, age: 30, firstName: sehwag, lastName: virender, gpa: 3.2
+
+
+*Sorting StudentRoster based on Gpa*
+
+id: 50, age: 26, firstName: sharma, lastName: rohit, gpa: 2.4
+id: 10, age: 28, firstName: rama, lastName: tej, gpa: 3.1
+id: 30, age: 30, firstName: sehwag, lastName: virender, gpa: 3.2
+id: 40, age: 29, firstName: peters, lastName: daniel, gpa: 3.8
+id: 20, age: 27, firstName: patel, lastName: Munaf, gpa: 3.9
+
+
+*Sorting PersonRoster based on ID*
+
+id: 60, age: 35, firstName: green, lastName: rachel, gpa: 3.8
+id: 70, age: 31, firstName: stinson, lastName: barney, gpa: 4.0
+id: 80, age: 34, firstName: nairobi, lastName: tokyo, gpa: 3.2
+id: 90, age: 33, firstName: komaram, lastName: bheem, gpa: 2.7
+id: 100, age: 32, firstName: kalyan, lastName: pavan, gpa: 3.3
+
+
+*Sorting PersonRoster based on Age*
+
+id: 70, age: 31, firstName: stinson, lastName: barney, gpa: 4.0
+id: 100, age: 32, firstName: kalyan, lastName: pavan, gpa: 3.3
+id: 90, age: 33, firstName: komaram, lastName: bheem, gpa: 2.7
+id: 80, age: 34, firstName: nairobi, lastName: tokyo, gpa: 3.2
+id: 60, age: 35, firstName: green, lastName: rachel, gpa: 3.8
+
+
+*Sorting PersonRoster based on LastName*
+
+id: 70, age: 31, firstName: stinson, lastName: barney, gpa: 4.0
+id: 90, age: 33, firstName: komaram, lastName: bheem, gpa: 2.7
+id: 100, age: 32, firstName: kalyan, lastName: pavan, gpa: 3.3
+id: 60, age: 35, firstName: green, lastName: rachel, gpa: 3.8
+id: 80, age: 34, firstName: nairobi, lastName: tokyo, gpa: 3.2
+
+
+*Sorting PersonRoster based on Gpa*
+
+id: 90, age: 33, firstName: komaram, lastName: bheem, gpa: 2.7
+id: 80, age: 34, firstName: nairobi, lastName: tokyo, gpa: 3.2
+id: 100, age: 32, firstName: kalyan, lastName: pavan, gpa: 3.3
+id: 60, age: 35, firstName: green, lastName: rachel, gpa: 3.8
+id: 70, age: 31, firstName: stinson, lastName: barney, gpa: 4.0
+
+*/
